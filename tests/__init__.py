12e4adba02878faf22d85389f7a68b637f236066
desbma <desbma@users.noreply.github.com>
2016-05-15 22:16:41 +0200

    Add tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,41 @@
 #!/usr/bin/env python3
 
+import datetime
+import inspect
 import logging
+import random
 import unittest
 
+import amg
 
 
 class TestAmg(unittest.TestCase):
-  pass
+
+  def test_get_reviews(self):
+    count = random.randint(10, 50)
+    gen = amg.get_reviews()
+    self.assertTrue(inspect.isgenerator(gen))
+    for i, review in zip(range(count), gen):
+      self.assertIsInstance(review, amg.ReviewMetadata)
+      self.assertTrue(review.url.startswith(amg.ROOT_URL))
+      self.assertIsInstance(review.artist, str)
+      self.assertTrue(review.artist)
+      self.assertIsInstance(review.album, str)
+      self.assertTrue(review.album)
+      self.assertTrue(review.cover_thumbnail_url.startswith("http"))
+      self.assertTrue(review.cover_url.startswith("http"))
+      self.assertIsInstance(review.date_published, datetime.date)
+      self.assertIsInstance(review.tags, tuple)
+      self.assertTrue(review.tags)
+      for tag in review.tags:
+        self.assertIsInstance(tag, str)
+    self.assertEqual(i, count - 1)
+
+  def test_get_embedded_track(self):
+    review_url = "https://www.angrymetalguy.com/vredehammer-violator-review/"
+    review_page = amg.fetch(review_url)
+    track_url = amg.get_embedded_track(review_page)
+    self.assertEqual(track_url, "https://www.youtube.com/watch?v=9Z34GAEO8hU")
 
 
 if __name__ == "__main__":
