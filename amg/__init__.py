2f074f26f145bc26823a7c29222a1e15a2998d59
desbma <desbma@users.noreply.github.com>
2016-07-04 00:11:27 +0200

    Auto scale covers with odd width or height

--- amg/__init__.py
+++ amg/__init__.py
@@ -204,6 +204,7 @@ def download_and_merge(review, track_url, tmp_dir):
          "-loglevel", "quiet",
          "-loop", "1", "-framerate", "1", "-i", img_filepath,
          "-f", "concat", "-i", concat_filepath,
+         "-filter:v", "scale=trunc(iw/2)*2:trunc(ih/2)*2",
          "-c:a", "copy",
          "-c:v", "libx264", "-crf", "18", "-tune:v", "stillimage", "-preset", "ultrafast",
          "-shortest",
