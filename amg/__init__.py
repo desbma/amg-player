a372c9a18186b3f0b57fd5e2f3faa0576781c000
desbma <desbma@users.noreply.github.com>
2016-08-28 16:26:49 +0200

    Caache review pages and display reviews having no track

--- amg/__init__.py
+++ amg/__init__.py
@@ -31,6 +31,7 @@ import cursesmenu
 import lxml.cssselect
 import lxml.etree
 import requests
+import web_cache
 import youtube_dl
 
 
@@ -56,13 +57,19 @@ PLAYER_IFRAME_SELECTOR = lxml.cssselect.CSSSelector("div.entry_content iframe")
 BANDCAMP_JS_SELECTOR = lxml.cssselect.CSSSelector("html > head > script")
 
 
-def fetch_page(url):
+def fetch_page(url, *, http_cache=None):
   """ Fetch page & parse it with LXML. """
-  logging.getLogger().debug("Fetching '%s'..." % (url))
-  response = requests.get(url, timeout=9.1)
-  response.raise_for_status()
-  page = response.content.decode("utf-8")
-  return lxml.etree.XML(page, HTML_PARSER)
+  if (http_cache is not None) and (url in http_cache):
+    logging.getLogger().info("Got data for URL '%s' from cache" % (url))
+    page = http_cache[url]
+  else:
+    logging.getLogger().debug("Fetching '%s'..." % (url))
+    response = requests.get(url, timeout=9.1)
+    response.raise_for_status()
+    page = response.content
+    if http_cache is not None:
+      http_cache[url] = page
+  return lxml.etree.XML(page.decode("utf-8"), HTML_PARSER)
 
 
 def fetch_ressource(url, dest_filepath):
@@ -110,33 +117,37 @@ def get_reviews():
       yield parse_review_block(review)
 
 
-def get_embedded_track(page):
+def get_embedded_track(page, http_cache):
   """ Parse page and extract embedded track. """
   url = None
   audio_only = False
   try:
-    iframe = PLAYER_IFRAME_SELECTOR(page)[0]
-    iframe_url = iframe.get("src")
-    if iframe_url is not None:
-      yt_prefix = "https://www.youtube.com/embed/"
-      bc_prefix = "https://bandcamp.com/EmbeddedPlayer/"
-      sc_prefix = "https://w.soundcloud.com/player/"
-      if iframe_url.startswith(yt_prefix):
-        yt_id = iframe_url[len(yt_prefix):]
-        url = "https://www.youtube.com/watch?v=%s" % (yt_id)
-      elif iframe_url.startswith(bc_prefix):
-        iframe_page = fetch_page(iframe_url)
-        js = BANDCAMP_JS_SELECTOR(iframe_page)[-1].text
-        js = next(filter(operator.methodcaller("__contains__",
-                                               "var playerdata ="),
-                         js.split("\n")))
-        js = js.split("=", 1)[1].rstrip(";" + string.whitespace)
-        js = json.loads(js)
-        url = js["linkback"]
-        audio_only = True
-      elif iframe_url.startswith(sc_prefix):
-        url = iframe_url.split("&", 1)[0]
-        audio_only = True
+    try:
+      iframe = PLAYER_IFRAME_SELECTOR(page)[0]
+    except IndexError:
+      pass
+    else:
+      iframe_url = iframe.get("src")
+      if iframe_url is not None:
+        yt_prefix = "https://www.youtube.com/embed/"
+        bc_prefix = "https://bandcamp.com/EmbeddedPlayer/"
+        sc_prefix = "https://w.soundcloud.com/player/"
+        if iframe_url.startswith(yt_prefix):
+          yt_id = iframe_url[len(yt_prefix):]
+          url = "https://www.youtube.com/watch?v=%s" % (yt_id)
+        elif iframe_url.startswith(bc_prefix):
+          iframe_page = fetch_page(iframe_url, http_cache=http_cache)
+          js = BANDCAMP_JS_SELECTOR(iframe_page)[-1].text
+          js = next(filter(operator.methodcaller("__contains__",
+                                                 "var playerdata ="),
+                           js.split("\n")))
+          js = js.split("=", 1)[1].rstrip(";" + string.whitespace)
+          js = json.loads(js)
+          url = js["linkback"]
+          audio_only = True
+        elif iframe_url.startswith(sc_prefix):
+          url = iframe_url.split("&", 1)[0]
+          audio_only = True
   except Exception as e:
     logging.getLogger().error("%s: %s" % (e.__class__.__qualname__, e))
   if url is not None:
@@ -226,48 +237,13 @@ def play(review, track_url, *, merge_with_picture):
     subprocess.check_call(cmd)
 
 
-def reviews_to_strings(reviews, known_reviews):
-  """ Generate a list of string representations of reviews. """
-  lines = []
-  for i, review in enumerate(reviews):
-    try:
-      last_played = known_reviews.getLastPlayed(review.url).strftime("%x %X")
-    except KeyError:
-      last_played = "never"
-    lines.append(("%s - %s" % (review.artist, review.album),
-                  "Published: %s" % (review.date_published.strftime("%x")),
-                  "Last played: %s" % (last_played)))
-  # auto align/justify
-  max_lens = [0] * len(lines[0])
-  for line in lines:
-    for i, s in enumerate(line):
-      if len(s) > max_lens[i]:
-        max_lens[i] = len(s)
-  sep = "\t"
-  for i, line in enumerate(lines):
-    lines[i] = "%s%s" % (" " if i < 9 else "",
-                         sep.join(s.ljust(max_len) for s, max_len in zip(line, max_lens)))
-  return lines
-
-
-def setup_and_show_menu(mode, reviews, known_reviews, selected_idx=None):
-  """ Setup and display interactive menu, return selected review index or None if exist requested. """
-  menu = AmgMenu(reviews=reviews,
-                 known_reviews=known_reviews,
-                 mode=mode,
-                 selected_idx=selected_idx)
-  menu.show()
-  idx = menu.selected_option
-  return None if (idx == len(reviews)) else idx
-
-
 class AmgMenu(cursesmenu.CursesMenu):
 
   """ Custom menu to choose review/track. """
 
   UserAction = enum.Enum("ReviewAction", ("DEFAULT", "OPEN_REVIEW"))
 
-  def __init__(self, *, reviews, known_reviews, mode, selected_idx):
+  def __init__(self, *, reviews, known_reviews, http_cache, mode, selected_idx):
     menu_subtitle = {PlayerMode.MANUAL: "Select a track to play",
                      PlayerMode.RADIO: "Select track to start playing from"}
     super().__init__("AMG Player v%s" % (__version__),
@@ -279,7 +255,7 @@ class AmgMenu(cursesmenu.CursesMenu):
                      True)
     if selected_idx is not None:
       self.current_option = selected_idx
-    review_strings = reviews_to_strings(reviews, known_reviews)
+    review_strings = __class__.reviewsToStrings(reviews, known_reviews, http_cache)
     for index, (review, review_string) in enumerate(zip(reviews, review_strings)):
       self.append_item(ReviewItem(review, review_string, index, self))
 
@@ -301,6 +277,49 @@ class AmgMenu(cursesmenu.CursesMenu):
   def get_last_user_action(self):
     """ Return last user action when item was selected. """
     return self.user_action
+
+  @staticmethod
+  def reviewsToStrings(reviews, known_reviews, http_cache):
+    """ Generate a list of string representations of reviews. """
+    lines = []
+    for i, review in enumerate(reviews):
+      try:
+        played = "Last played: %s" % (known_reviews.getLastPlayed(review.url).strftime("%x %X"))
+      except KeyError:
+        if review.url in http_cache:
+          review_page = fetch_page(review.url, http_cache=http_cache)
+          if get_embedded_track(review_page, http_cache)[0] is None:
+            played = "No track"
+          else:
+            played = "Last played: never"
+        else:
+          played = "Last played: never"
+      lines.append(("%s - %s" % (review.artist, review.album),
+                    "Published: %s" % (review.date_published.strftime("%x")),
+                    played))
+    # auto align/justify
+    max_lens = [0] * len(lines[0])
+    for line in lines:
+      for i, s in enumerate(line):
+        if len(s) > max_lens[i]:
+          max_lens[i] = len(s)
+    sep = "\t"
+    for i, line in enumerate(lines):
+      lines[i] = "%s%s" % (" " if i < 9 else "",
+                           sep.join(s.ljust(max_len) for s, max_len in zip(line, max_lens)))
+    return lines
+
+  @staticmethod
+  def setupAndShow(mode, reviews, known_reviews, http_cache, selected_idx=None):
+    """ Setup and display interactive menu, return selected review index or None if exist requested. """
+    menu = AmgMenu(reviews=reviews,
+                   known_reviews=known_reviews,
+                   http_cache=http_cache,
+                   mode=mode,
+                   selected_idx=selected_idx)
+    menu.show()
+    idx = menu.selected_option
+    return None if (idx == len(reviews)) else idx
 
 
 class ReviewItem(cursesmenu.items.SelectionItem):
@@ -375,9 +394,22 @@ def cl_main():
   known_reviews = KnownReviews()
   reviews = list(itertools.islice(get_reviews(), args.count))
 
+  # http cache
+  cache_dir = appdirs.user_cache_dir("amg-player")
+  os.makedirs(cache_dir, exist_ok=True)
+  cache_filepath = os.path.join(cache_dir, "http_cache.db")
+  http_cache = web_cache.WebCache(cache_filepath,
+                                  "reviews",
+                                  caching_strategy=web_cache.CachingStrategy.FIFO,
+                                  expiration=60 * 60 * 24 * 30 * 3,  # 3 months
+                                  compression=web_cache.Compression.DEFLATE)
+  purged_count = http_cache.purge()
+  row_count = len(http_cache)
+  logging.getLogger().debug("HTTP Cache contains %u entries (%u removed)" % (row_count, purged_count))
+
   # initial menu
   if args.mode in (PlayerMode.MANUAL, PlayerMode.RADIO):
-    selected_idx = setup_and_show_menu(args.mode, reviews, known_reviews)
+    selected_idx = AmgMenu.setupAndShow(args.mode, reviews, known_reviews, http_cache)
 
   to_play = None
   track_loop = True
@@ -407,8 +439,8 @@ def cl_main():
         break
 
     # fetch review & play
-    review_page = fetch_page(review.url)
-    track_url, audio_only = get_embedded_track(review_page)
+    review_page = fetch_page(review.url, http_cache=http_cache)
+    track_url, audio_only = get_embedded_track(review_page, http_cache)
     if track_url is None:
       logging.getLogger().warning("Unable to extract embedded track")
     else:
@@ -445,7 +477,7 @@ def cl_main():
 
     if track_loop and (args.mode is PlayerMode.MANUAL):
       # update menu and display it
-      selected_idx = setup_and_show_menu(args.mode, reviews, known_reviews, selected_idx=selected_idx)
+      selected_idx = AmgMenu.setupAndShow(args.mode, reviews, known_reviews, http_cache, selected_idx=selected_idx)
 
 
 if __name__ == "__main__":
