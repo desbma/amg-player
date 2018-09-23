da37e6a783ea63e164003a171aa52f2afa275a35
desbma <desbma@users.noreply.github.com>
2018-09-24 00:07:28 +0200

    Officially support Python 3.7

--- setup.py
+++ setup.py
@@ -47,5 +47,6 @@ setup(name="amg-player",
                    "Programming Language :: Python :: 3.4",
                    "Programming Language :: Python :: 3.5",
                    "Programming Language :: Python :: 3.6",
+                   "Programming Language :: Python :: 3.7",
                    "Topic :: Multimedia :: Sound/Audio :: Players",
                    "Topic :: Utilities"])
