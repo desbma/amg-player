c23bf89070006302aaabc63aacb63d6f3bb669d8
desbma <desbma@users.noreply.github.com>
2019-01-25 20:33:26 +0100

    Version 2019.01.25.0

--- amg/__init__.py
+++ amg/__init__.py
@@ -2,7 +2,7 @@
 
 """ Browse & play embedded tracks from Angry Metal Guy music reviews. """
 
-__version__ = "2018.12.15.0"
+__version__ = "2019.01.25.0"
 __author__ = "desbma"
 __license__ = "GPLv3"
 
