c0631952874e7db54bf2fe3e24a3b646ba427b48
desbma <desbma@users.noreply.github.com>
2025-11-16 21:21:10 +0100

    Version 2025.11.16.1

--- amg/__init__.py
+++ amg/__init__.py
@@ -2,7 +2,7 @@
 
 """Browse & play embedded tracks from Angry Metal Guy music reviews."""
 
-__version__ = "2025.11.16.0"
+__version__ = "2025.11.16.1"
 __author__ = "desbma"
 __license__ = "GPLv3"
 
